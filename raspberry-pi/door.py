ffe6024500ce9b16743745d11e8aa93dd68cf097
Udantha Pathirana <info@udantha.com>
2019-09-27 20:17:29 +0530

    added door control

---
+++ raspberry-pi/door.py
@@ -0,0 +1,25 @@
+import RPi.GPIO as GPIO
+import time
+import sys
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.OUT)
+pwm = GPIO.PWM(18, 50)
+pwm.start(7.5)
+
+
+try:
+        #open
+        pwm.ChangeDutyCycle(7.5)  # turn towards 90 degree #Open 
+        time.sleep(1)
+        #close
+        pwm.ChangeDutyCycle(2.5)  # turn towards 0 degree
+        time.sleep(1) # sleep 1 second
+
+#except KeyboardInterrupt:
+        pwm.stop()
+        GPIO.cleanup()
+
+
+except IOError as e:
+    print "I/O error({0}): {1}".format(e.errno, e.strerror)

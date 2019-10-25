785395b04519d60332fc1f038474b43cdec88096
Udantha Pathirana <info@udantha.com>
2019-10-25 14:33:28 +0530

    sensor readings accumulated

--- raspberry-pi/move.py
+++ raspberry-pi/move.py
@@ -6,52 +6,96 @@ from time import sleep
 MQTT_SERVER = "192.168.1.5"
 MQTT_CHANNEL_GARBAGE = "channel_garbage_amount"
 GARBAGE_BIN_ID = "20"
+GARBAGE_BIN_TYPE_METAL = "BIN_METAL"
+GARBAGE_BIN_TYPE_ALL = "BIN_NON_METAL"
 
 # Pin numbers
 PIN_US_TRIGGER = 12
 PIN_US_ECHO = 18
-PIN_DOOR_MOTOR = 03
+PIN_MOTOR_DOOR = 12
+PIN_MOTOR_MAIN = 11
+PIN_IR_TRIGGER = 16
+
+GPIO.setmode(GPIO.BOARD)
+
+# IR Sensor
+GPIO.setup(PIN_IR_TRIGGER, GPIO.IN)
+# End IR Sensor
 
 # Start configure pins for Door motor
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(PIN_DOOR_MOTOR, GPIO.OUT) # output to send our PWM signal on
-pwm=GPIO.PWM(PIN_DOOR_MOTOR, 50) # setup PWM on pin #3 at 50Hz
-pwm.start(0) # start it with 0 duty cycle so it doesn't set any angles on startup
+GPIO.setup(PIN_MOTOR_DOOR, GPIO.OUT)  # output to send our PWM signal on
+motorGateDoor = GPIO.PWM(PIN_MOTOR_DOOR, 50)  # setup PWM on pin #3 at 50Hz
+# start it with 0 duty cycle so it doesn't set any angles on startup
+motorGateDoor.start(0)
 # End Door motor
+
+# Main motor
+GPIO.setup(PIN_MOTOR_MAIN, GPIO.OUT)
+motorMain = GPIO.PWM(PIN_MOTOR_MAIN, 50)
+motorMain.start(0)
+# End Main motor
+
 # Start configure pins for distance
 GPIO.setup(PIN_US_TRIGGER, GPIO.OUT)
 GPIO.output(PIN_US_TRIGGER, False)
 GPIO.setup(PIN_US_ECHO, GPIO.IN)
 # End for distance
 
-# move main motor to bin
+# Listen for IR break to catch Initiation
+try:
+    while True:
+        if GPIO.input(PIN_IR_TRIGGER):
+            # 1. Initiate Metal detector
+            # Set the target bin
+            targetBinType = GARBAGE_BIN_TYPE_ALL  # or GARBAGE_BIN_TYPE_METAL based on
 
+            # 2. decide which way to turn and Turn
+            if targetBinType == GARBAGE_BIN_TYPE_METAL:
+                # turn the opening towards metal
+                motorMain.ChangeDutyCycle(7.5)  # turn towards 90 degree
+                time.sleep(0.5)
 
-# open door by rotating door motor
-SetAngle(90)
-sleep(3)
-SetAngle(0)
-pwm.stop()
+            # 3. Open gate and drop garbage
+            # open
+            motorGateDoor.ChangeDutyCycle(7.5)  # turn towards 90 degree #Open
+            time.sleep(1)
+            # close
+            motorGateDoor.ChangeDutyCycle(2.5)  # turn towards 0 degree
+            time.sleep(1)  # sleep 1 second
+            motorGateDoor.stop()
 
-# fire up calculating measurement
-distance = getGarbageMeasurement()
+            # 4. Get garbage size
+            # fire up calculating measurement
+            distance = getGarbageMeasurement()
 
-# broadcast measurement via MQTT
-publish.single(MQTT_CHANNEL_GARBAGE, GARBAGE_BIN_ID + ':' + str(distance), hostname=MQTT_SERVER)
+            # broadcast measurement via MQTT
+            publish.single(MQTT_CHANNEL_GARBAGE, GARBAGE_BIN_ID + ':' + str(distance) + ':' + targetBinType, hostname=MQTT_SERVER)
 
-# move back to starting position
+            # 5. back to original position
+            # turn the opening towards default all position
+            motorMain.ChangeDutyCycle(2.5)  # turn towards 0 degree
+            time.sleep(0.5)
+            motorMain.stop()
 
+            while GPIO.input(PIN_IR_TRIGGER):
+                time.sleep(0.2)
+
+except KeyboardInterrupt:
+    GPIO.cleanup()
 
 GPIO.cleanup()
 
 # === method registrations ===
-def SetAngle(angle):
-    duty = angle / 18 + 2
-    GPIO.output(PIN_DOOR_MOTOR, True)
-    pwm.ChangeDutyCycle(duty)
-    sleep(1)
-    GPIO.output(PIN_DOOR_MOTOR, False)
-    pwm.ChangeDutyCycle(0)
+
+
+# def SetAngle(angle):
+#     duty = angle / 18 + 2
+#     GPIO.output(PIN_DOOR_MOTOR, True)
+#     pwm.ChangeDutyCycle(duty)
+#     sleep(1)
+#     GPIO.output(PIN_DOOR_MOTOR, False)
+#     pwm.ChangeDutyCycle(0)
+
 
 def getGarbageMeasurement():
     # Waiting For Sensor1 To Settle
@@ -63,7 +107,7 @@ def getGarbageMeasurement():
     while GPIO.input(PIN_US_ECHO) == 0:
         pass
         pulse_start1 = time.time()
-    
+
     while GPIO.input(PIN_US_ECHO) == 1:
         pass
         pulse_end1 = time.time()
@@ -71,6 +115,6 @@ def getGarbageMeasurement():
     pulse_duration1 = pulse_end1 - pulse_start1
 
     distanceInCM = pulse_duration1 * 17150
-    distanceInCM= round(distanceInCM, 2)
+    distanceInCM = round(distanceInCM, 2)
     time.sleep(10)
     return distanceInCM

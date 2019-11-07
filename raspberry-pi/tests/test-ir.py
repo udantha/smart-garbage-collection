ca9d9970df1ee744216b60ec2add24d788b209c0
Udantha Pathirana <info@udantha.com>
2019-11-07 08:09:39 +0530

    Testing modifications

--- raspberry-pi/tests/test-ir.py
+++ raspberry-pi/tests/test-ir.py
@@ -8,7 +8,7 @@ PIN_IR_RECEIVER = 16
 
 # IR Sensor
 GPIO.setup(PIN_IR_TRIGGER, GPIO.OUT)
-GPIO.output(PIN_IR_TRIGGER, False)
+GPIO.output(PIN_IR_TRIGGER, True)
 GPIO.setup(PIN_IR_RECEIVER, GPIO.IN)
 # End IR Sensor
 
@@ -16,10 +16,10 @@ GPIO.setup(PIN_IR_RECEIVER, GPIO.IN)
 # Listen for IR break to catch Initiation
 try:
     while True:
-        GPIO.output(PIN_IR_TRIGGER, True)
+        #GPIO.output(PIN_IR_TRIGGER, True)
         print "Output - " + str(GPIO.input(PIN_IR_RECEIVER))
         time.sleep(0.2)
-        GPIO.output(PIN_IR_TRIGGER, False)
+        #GPIO.output(PIN_IR_TRIGGER, False)
 
         # if GPIO.input(PIN_IR_RECEIVER):
         #     print "Object Detected"

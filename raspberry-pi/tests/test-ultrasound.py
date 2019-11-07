2e837899a4054d745276f18196e5aefef1e133b4
Udantha Pathirana <info@udantha.com>
2019-11-07 23:40:09 +0530

    Testing modifications

---
+++ raspberry-pi/tests/test-ultrasound.py
@@ -0,0 +1,37 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+
+# Pin numbers
+PIN_US_TRIGGER = 12
+PIN_US_ECHO = 11
+
+try:
+    print "Height in CM - " + str(getGarbageMeasurement())
+except KeyboardInterrupt:
+    GPIO.cleanup()
+
+GPIO.cleanup()
+
+def getGarbageMeasurement():
+    # Waiting For Sensor1 To Settle
+    time.sleep(.1)
+    GPIO.output(PIN_US_TRIGGER, True)
+    time.sleep(0.00001)
+    GPIO.output(PIN_US_TRIGGER, False)
+
+    while GPIO.input(PIN_US_ECHO) == 0:
+        pass
+        pulse_start1 = time.time()
+
+    while GPIO.input(PIN_US_ECHO) == 1:
+        pass
+        pulse_end1 = time.time()
+
+    pulse_duration1 = pulse_end1 - pulse_start1
+
+    distanceInCM = pulse_duration1 * 17150
+    distanceInCM = round(distanceInCM, 2)
+    time.sleep(10)
+    return distanceInCM

b5deda5daccd9f7294d7b3455e6869bfcbe705d8
Udantha Pathirana <info@udantha.com>
2019-11-07 05:55:47 +0530

    testing

--- raspberry-pi/tests/test-ir.py
+++ raspberry-pi/tests/test-ir.py
@@ -15,8 +15,9 @@ GPIO.setup(PIN_IR_RECEIVER, GPIO.IN)
 # Listen for IR break to catch Initiation
 try:
     while True:
-        GPIO.output(PIN_IR_TRIGGER, True)
+        #GPIO.output(PIN_IR_TRIGGER, True)
         print "Output - " + str(GPIO.input(PIN_IR_RECEIVER))
+        time.sleep(0.2)
         # if GPIO.input(PIN_IR_RECEIVER):
         #     print "Object Detected"
         #     while GPIO.input(PIN_IR_RECEIVER):

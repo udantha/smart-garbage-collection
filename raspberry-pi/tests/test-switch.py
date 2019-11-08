8b5988c6c4e1d832ec313f0e06649b84aeb790e2
Udantha Pathirana <info@udantha.com>
2019-11-08 22:21:14 +0530

    Testing modifications

---
+++ raspberry-pi/tests/test-switch.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+
+# Pin numbers
+PIN_SWITCH = 38
+
+while True:
+    input_state = GPIO.input(PIN_SWITCH)
+    if input_state == False:
+        print('Button Pressed')
+        time.sleep(0.2)
+
+GPIO.cleanup()

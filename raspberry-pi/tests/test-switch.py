d048668b57cc29b1c94e2fd5e65f55309e82a94f
Udantha Pathirana <info@udantha.com>
2019-11-08 22:22:08 +0530

    Testing modifications

--- raspberry-pi/tests/test-switch.py
+++ raspberry-pi/tests/test-switch.py
@@ -6,6 +6,8 @@ GPIO.setmode(GPIO.BOARD)
 # Pin numbers
 PIN_SWITCH = 38
 
+GPIO.setup(PIN_SWITCH, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
 while True:
     input_state = GPIO.input(PIN_SWITCH)
     if input_state == False:
